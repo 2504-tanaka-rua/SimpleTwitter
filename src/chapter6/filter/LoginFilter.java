dcaeb3c6a4edda64f8afe37d07f06571f8c4dcb0
tanaka_rua <tanaka.rua@alhinc.jp>
2025-05-13 16:09:54 +0900

    申し訳ありません。返信用に追加したクラスを追加でコミットさせていただきます。

---
+++ src/chapter6/filter/LoginFilter.java
@@ -0,0 +1,56 @@
+package chapter6.filter;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import chapter6.beans.User;
+
+@WebFilter(urlPatterns = {"/edit", "/setting"})
+public class LoginFilter implements Filter {
+
+	public static String INIT_PARAMETER_NAME_ENCODING = "encoding";
+
+	public static String DEFAULT_ENCODING = "UTF-8";
+
+	@Override
+	public void doFilter(ServletRequest request, ServletResponse response,
+			FilterChain chain) throws IOException, ServletException {
+
+		HttpServletRequest httpRequest = (HttpServletRequest) request;
+		HttpServletResponse httpResponse = (HttpServletResponse) response;
+
+		HttpSession session = httpRequest.getSession();
+		List<String> errorMessages = new ArrayList<String>();
+
+		User user = (User)httpRequest.getSession().getAttribute("loginUser");
+		if (user != null) {
+			chain.doFilter(request, response);
+		} else {
+			errorMessages.add("ログインしてください");
+			session.setAttribute("errorMessages", errorMessages);
+			httpResponse.sendRedirect("./login");
+			return;
+		}
+
+	}
+
+	@Override
+	public void init(FilterConfig config) {
+	}
+
+	@Override
+	public void destroy() {
+	}
+}
